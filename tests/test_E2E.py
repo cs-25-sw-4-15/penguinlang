637ba81fa460359b21e16c8faa08464e7364761e
naitsa <naitsabot@gmail.com>
2025-05-08 14:16:50 +0200

    force path

--- tests/test_E2E.py
+++ tests/test_E2E.py
@@ -1,4 +1,9 @@
+import pytest
+import sys
 import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import glob
 from pyboy import PyBoy
 from src.compiler import full_compile
